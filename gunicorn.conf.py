620f61290f30d744ab3d1d020999bb2a6b17ffd0
shantam balde <shantam.balde@bezoarsoftware.com>
2017-11-21 14:16:02 +0530

    First

--- gunicorn.conf.py
+++
@@ -1,5 +0,0 @@
-bind = '127.0.1.1:5000'
-worders = (os.sysconf('SC_NPROCESSORS_ONLN') * 2) + 1
-loglevel = 'error'
-command = '/usr/local/lib/python2.7/dist-packages/gunicorn'
-pythonpath = '/home/bezoar-developer-shantam/My-Projects/delbot'
