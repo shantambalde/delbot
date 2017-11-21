a1803feeebcb03a9653892c28b0ddde68496c21c
shantam balde <shantam.balde@bezoarsoftware.com>
2017-11-21 11:19:04 +0530

    123

--- delbot/settings.py
+++
@@ -1,7 +0,0 @@
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-ROOT_URLCONF = 'delbot.urls'
-
-WSGI_APPLICATION = 'delbot.wsgi.application'
-
-STATIC_URL = '/static/'
--- delbot/wsgi.py
+++ delbot/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "delbot.settings")
