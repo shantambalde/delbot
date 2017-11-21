a6b8289a317b595f9f3aff379d2a01f617aca53e
shantam balde <shantam.balde@bezoarsoftware.com>
2017-11-21 11:19:17 +0530

    123

---
+++ delbot/settings/base.py
@@ -0,0 +1,7 @@
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+ROOT_URLCONF = 'delbot.urls'
+
+WSGI_APPLICATION = 'delbot.wsgi.application'
+
+STATIC_URL = '/static/'
---
+++ delbot/settings/production.py
@@ -0,0 +1,5 @@
+DEBUG = False
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
