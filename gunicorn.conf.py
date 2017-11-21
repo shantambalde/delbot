0b856bd587a90127c5f9453868eb7f01210cc0d6
shantam balde <shantam.balde@bezoarsoftware.com>
2017-11-21 11:33:47 +0530

    Make it better

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,4 @@
-bind = '0.0.0.0:5000'
+bind = '127.0.1.1:5000'
 worders = (os.sysconf('SC_NPROCESSORS_ONLN') * 2) + 1
 loglevel = 'error'
 command = '/usr/local/lib/python2.7/dist-packages/gunicorn'
